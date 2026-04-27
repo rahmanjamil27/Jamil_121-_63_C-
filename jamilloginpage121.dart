d3cb868f16698501caa2e60af64926f176a0a850
Jamilur Rahman <rahmanjamil2709@gmail.com>
2026-04-27 20:45:29 +0600

    Create jamilloginpage121.dart

---
+++ jamilloginpage121.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() {
+    return _LoginPageState();
+  }
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Login Page")),
+      body: Center(
+        child: SizedBox(
+          height: 400,
+          width: 300,
+          child: Card(
+            color: const Color.fromARGB(255, 183, 205, 216),
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.lock_person),
+                      SizedBox(width: 8),
+                      Text("User Login"),
+                    ],
+                  ),
+                  SizedBox(height: 16),
+                  Form(
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          controller: emailController,
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(50)),
+                            ),
+                            hintText: "Enter Email",
+                            labelText: "Email",
+                            prefixIcon: Icon(Icons.email),
+                          ),
+                        ),
+                        SizedBox(height: 12),
+                        TextFormField(
+                          controller: passwordController,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(50)),
+                            ),
+                            hintText: "Enter Password",
+                            labelText: "Password",
+                            prefixIcon: Icon(Icons.lock),
+                          ),
+                        ),
+                        SizedBox(height: 16),
+                        ElevatedButton(
+                          onPressed: () {},
+                          child: Text("Login"),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
